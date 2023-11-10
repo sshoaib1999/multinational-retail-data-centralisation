793a568de6d0f1dd36a5fc4a5b8e61e4670e3e64
sshoaib1999 <sshoaib1999@hotmail.com>
2023-11-10 21:38:41 +0000

    Add files via upload
    milestone 2

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,71 +1,154 @@
-# data_cleaning.py
-
-import numpy as np
-
+import pandas as pd
+import re
 
 class DataCleaning:
+    def clean_user_data(self, user_data):
+        """
+        Clean user data by handling NULL values, date errors, incorrectly typed values, and incorrect information.
 
-    @staticmethod
-    def clean_user_data(df):
-        # Handle NULL values (for this example, we fill them with NaN)
-        df = df.replace({None: np.nan})
+        Args:
+            user_data (pandas.DataFrame): User data to be cleaned.
 
-        # Convert date columns to datetime type (replace 'your_date_column' with the actual column name)
-        df['your_date_column'] = pd.to_datetime(df['your_date_column'], errors='coerce')
+        Returns:
+            pandas.DataFrame: Cleaned user data.
+        """
+        # Handle NULL values
+        user_data.dropna(inplace=True)
 
-        # Handle other data cleaning steps as needed...
+        # Handle date errors (if applicable)
+        # For example, convert date columns to the correct date format
 
-        return df
+        # Handle incorrectly typed values and incorrect information
+        # You may apply specific data type conversions and filtering logic here
+
+        return user_data
     
-    @staticmethod
-    def clean_card_data(df):
-        # Replace NULL values with NaN (or handle them appropriately)
-        df = df.replace({None: np.nan, 'NULL': np.nan})
+    def validate_card_number(card_number):
+    # Remove any whitespace or hyphens from the card number
+        card_number = card_number.replace(" ", "").replace("-", "")
 
-        # Add specific cleaning steps, such as:
-        # - Formatting corrections
-        # - Data type conversions
-        # - Removal of erroneous values
-        # Note: This is a generic placeholder. Actual steps would depend on the nature of the data and its anomalies.
-
-        return df
+    # Check if the card number consists of digits and falls within a specified length range
+        if card_number.isdigit() and 12 <= len(card_number) <= 19:
+            return True
+        else:
+            return False
     
-    @staticmethod
-    def convert_product_weights(df):
-        def convert_weight(weight):
-            if isinstance(weight, str):
-                weight = weight.strip()
-                if weight.endswith("ml"):
-                    # Assuming a rough 1:1 conversion from ml to g
-                    weight = float(weight[:-2]) / 1000  # Convert to kg
-                elif weight.endswith("g"):
-                    weight = float(weight[:-1]) / 1000  # Convert to kg
-                try:
-                    return float(weight)
-                except ValueError:
+    def clean_card_data(self, card_data):
+        """
+        Clean the extracted card data by removing erroneous values, NULL values, or formatting errors.
+
+        Args:
+            card_data (pandas.DataFrame): Extracted card data.
+
+        Returns:
+            pandas.DataFrame: Cleaned card data.
+        """
+        # Remove rows with NULL values
+        card_data = card_data.dropna()
+
+        # Additional data cleaning steps as needed
+        # For example, you can perform formatting checks, remove duplicates, etc.
+
+        # Example: Remove rows with invalid card numbers (you can implement your own validation logic)
+        card_data = card_data[card_data['Card Number'].apply(validate_card_number)]
+
+        return card_data
+    
+    def _clean_store_data(self, store_data):
+        if store_data is None:
+            return None
+
+        # Example cleaning logic
+        cleaned_data = store_data.copy()
+
+        # Remove rows with missing values (NULL)
+        cleaned_data.dropna(inplace=True)
+
+        # Clean data type conversions or other cleaning operations
+
+        return cleaned_data
+    
+    def convert_product_weights(self, products_df):
+        try:
+            # Create a function to clean and convert weights
+            def clean_and_convert_weight(weight):
+                if pd.notna(weight):
+                    # Remove non-alphanumeric characters and whitespace
+                    weight = re.sub(r'[^0-9a-zA-Z.]', '', str(weight))
+
+                    # Define conversion factors for ml and g to kg
+                    conversion_factors = {
+                        'g': 0.001,
+                        'ml': 0.001,
+                    }
+
+                    # Extract unit from the weight (e.g., g, ml)
+                    unit = re.search(r'[a-zA-Z]+', weight)
+                    if unit:
+                        unit = unit.group().lower()
+                        # Check if the unit is in the conversion factors
+                        if unit in conversion_factors:
+                            # Convert the weight to kg
+                            weight_in_kg = float(re.sub(r'[a-zA-Z]+', '', weight)) * conversion_factors[unit]
+                            return weight_in_kg
+                    # If no unit is found or unit is not in the conversion factors, return NaN
                     return None
-            return weight
+                return None
 
-        df['weight'] = df['weight'].apply(convert_weight)
-        return df
-    
-    @staticmethod
-    def clean_products_data(df):
-        # Add other cleaning steps as necessary
-        # For example, removing rows with missing values
-        df = df.dropna()
-        return df
-    
-    @staticmethod
-    def clean_orders_data(df):
-        # Remove unnecessary columns
-        df = df.drop(columns=['first_name', 'last_name', '1'])
+            # Apply the clean_and_convert_weight function to the weight column
+            products_df['weight'] = products_df['weight'].apply(clean_and_convert_weight)
+
+            return products_df
+        except Exception as e:
+            print(f"Error converting product weights: {str(e)}")
+            return None
         
-        # You can add additional cleaning steps specific to the orders data here
+    def clean_products_data(self, products_df):
+        try:
+            # Clean the DataFrame from additional erroneous values
+            # You can perform specific cleaning operations based on your data's requirements here.
+            # For example, you can remove rows with missing or invalid values, duplicates, etc.
+            # Here's an example of removing rows with missing product names and duplicates based on the 'product_code' column:
+            cleaned_products_df = products_df.dropna(subset=['product_name'])
+            cleaned_products_df = cleaned_products_df.drop_duplicates(subset=['product_code'])
 
-        return df
-    
-    
-    
+            return cleaned_products_df
+        except Exception as e:
+            print(f"Error cleaning products data: {str(e)}")
+            return None
+        
+    def clean_orders_data(self, orders_data):
+        """
+        Clean the orders data by removing unnecessary columns.
 
-    
+        Args:
+            orders_data (pandas.DataFrame): Orders data to be cleaned.
+
+        Returns:
+            pandas.DataFrame: Cleaned orders data.
+        """
+        # Remove the columns 'first_name', 'last_name', and '1'
+        columns_to_remove = ['first_name', 'last_name', '1']
+        cleaned_orders_data = orders_data.drop(columns=columns_to_remove, errors='ignore')
+
+        return cleaned_orders_data
+
+
+
+
+
+
+
+    def clean_csv_data(self, data):
+        # Add code to clean data extracted from CSV
+        pass
+
+    def clean_api_data(self, data):
+        # Add code to clean data extracted from an API
+        pass
+
+    def clean_s3_data(self, data):
+        # Add code to clean data extracted from an S3 bucket
+        pass
+
+# You'll define methods for data cleaning from each data source when required.
--- data_extraction.py
+++ data_extraction.py
@@ -1,9 +1,8 @@
-# data_extraction.py
-
 import pandas as pd
+from database_utils import DatabaseConnector
+import tabula
 import requests
 import boto3
-import tabula
 
 
 class DataExtractor:
@@ -11,88 +10,119 @@ class DataExtractor:
     def __init__(self, db_connector):
         self.db_connector = db_connector
 
+    def read_rds_data(self, table_name):
+        engine = self.db_connector.get_engine()
+        # Check if the table name exists in the list of tables
+        if table_name not in self.db_connector.list_db_tables(engine):
+            raise ValueError(f"Table '{table_name}' does not exist in the database.")
+
+        # Read data from the table using the DatabaseConnector
+        engine = self.db_connector.get_engine()
+        try:
+            connection = engine.connect()
+            query = f"SELECT * FROM {table_name}"
+            data = pd.read_sql(query, connection)
+            connection.close()
+            return data
+        except Exception as e:
+            print(f"Error reading data from the table: {str(e)}")
+            return None
+        
     def read_rds_table(self, table_name):
-        engine = self.db_connector.init_db_engine()
-        df = pd.read_sql(f"SELECT * FROM {table_name}", engine)
-        return df
-    
-    def retrieve_pdf_data(self, link):
-        # Use tabula to read the PDF into a DataFrame
-        # Here, we assume the PDF has tables that tabula can recognize and read
-        # We also assume the PDF can have multiple tables across multiple pages, hence pages='all'
-        dfs = tabula.read_pdf(link, pages='all', multiple_tables=True)
+        """
+        Extract a database table to a Pandas DataFrame.
 
-        # Concatenate all tables into one DataFrame
-        df = pd.concat(dfs, ignore_index=True)
+        Args:
+            table_name (str): Name of the table to extract.
 
-    def list_number_of_stores(self, endpoint, headers):
-        response = requests.get(endpoint, headers=headers)
-        if response.status_code == 200:
-            return response.json()['number_of_stores']  # Assuming the response has a key 'number_of_stores' containing the count
-        else:
+        Returns:
+            pandas.DataFrame: Data from the specified table.
+        """
+        # Check if the table name exists in the list of tables
+        if table_name not in self.db_connector.list_db_tables():
+            raise ValueError(f"Table '{table_name}' does not exist in the database.")
+
+        # Read data from the table using the DatabaseConnector
+        engine = self.db_connector.get_engine()
+        try:
+            data = pd.read_sql_table(table_name, con=engine)
+            return data
+        except Exception as e:
+            print(f"Error reading data from the table: {str(e)}")
             return None
         
-    def retrieve_stores_data(self, base_endpoint, headers):
-        number_of_stores = self.list_number_of_stores("https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores", headers)
-        all_stores = []
-        
-        for store_number in range(1, number_of_stores + 1):
-            endpoint = f"{base_endpoint}/{store_number}"
-            response = requests.get(endpoint, headers=headers)
+    def retrieve_pdf_data(self, pdf_link):
+        """
+        Extract data from a PDF document and return it as a Pandas DataFrame.
+
+        Args:
+            pdf_link (str): Link to the PDF document.
+
+        Returns:
+            pandas.DataFrame: Extracted data from the PDF.
+        """
+        try:
+            # Use tabula to extract data from all pages of the PDF
+            extracted_data = tabula.read_pdf(pdf_link, pages="all")
+
+            # Combine data from all pages into a single DataFrame
+            combined_data = pd.concat(extracted_data, ignore_index=True)
+
+            return combined_data
+        except Exception as e:
+            print(f"Error extracting data from PDF: {str(e)}")
+            return None
+
+    def list_number_of_stores(self, number_of_stores_endpoint, headers):
+        try:
+            response = requests.get(number_of_stores_endpoint, headers=headers)
             if response.status_code == 200:
-                all_stores.append(response.json())
-        
-        return pd.DataFrame(all_stores)
-    
-    @staticmethod
-    def _clean_store_data(df):
-        # Handle cleaning operations specific to the store data. For example:
-        df = df.dropna()  # Drops rows with missing values
-        # Add other cleaning steps as necessary...
-        
-        return df
-        
+                data = response.json()  # Assuming the response is in JSON format
+                return data.get('number_of_stores', 0)
+            else:
+                print(f"Failed to retrieve the number of stores. Status code: {response.status_code}")
+        except Exception as e:
+            print(f"Error while retrieving the number of stores: {str(e)}")
+        return 0  # Return 0 if there's an error
+
+    def retrieve_stores_data(self, retrieve_store_endpoint, headers):
+        try:
+            response = requests.get(retrieve_store_endpoint, headers=headers)
+            if response.status_code == 200:
+                data = response.json()  # Assuming the response is in JSON format
+                # Assuming the data is a list of stores
+                stores_data = pd.DataFrame(data)
+                return stores_data
+            else:
+                print(f"Failed to retrieve store data. Status code: {response.status_code}")
+        except Exception as e:
+            print(f"Error while retrieving store data: {str(e)}")
+        return None  # Return None if there's an error
+
     def extract_from_s3(self, s3_address):
-        s3 = boto3.client('s3')
-        bucket_name = s3_address.split('//')[1].split('/')[0]
-        file_name = s3_address.split('/')[-1]
-
         try:
-            s3.download_file(bucket_name, file_name, file_name)
-            df = pd.read_csv(file_name)
-            return df
+            # Initialize an S3 client using AWS CLI credentials
+            s3 = boto3.client('s3')
+
+            # Split the S3 address to extract the bucket name and file path
+            s3_parts = s3_address.replace("s3://", "").split("/")
+            bucket_name = s3_parts[0]
+            file_path = "/".join(s3_parts[1:])
+
+            # Download the CSV file from S3
+            s3_object = s3.get_object(Bucket=bucket_name, Key=file_path)
+            data = s3_object['Body'].read()
+
+            # Convert the data to a Pandas DataFrame
+            product_data = pd.read_csv(BytesIO(data))
+
+            return product_data
         except Exception as e:
-            print(f"An error occurred while downloading and extracting from S3: {e}")
+            print(f"Error extracting data from S3: {str(e)}")
             return None
+
         
-    def extract_and_load_from_s3(self, s3_address, db_connector, table_name):
-        s3 = boto3.client('s3')
-        bucket_name = s3_address.split('//')[1].split('/')[0]
-        file_name = s3_address.split('/')[-1]
 
-        try:
-            # Download the JSON file from S3
-            s3.download_file(bucket_name, file_name, file_name)
+        
 
-            # Read and parse the JSON data
-            df = pd.read_json(file_name)
-
-            # Clean and transform the data (if needed)
-            df = self._clean_json_data(df)  # Define your cleaning method
-
-            # Connect to the database
-            engine = db_connector.init_db_engine()
-
-            # Insert the cleaned data into the database table
-            df.to_sql(table_name, engine, if_exists='replace', index=False)  # Adjust 'if_exists' as needed
-
-            return True
-        except Exception as e:
-            print(f"An error occurred while downloading, processing, and loading from S3: {e}")
-            return False
-
-    def _clean_json_data(self, df):
-        # Handle cleaning operations specific to the JSON data. For example:
-        df = df.dropna()  # Drops rows with missing values
-        # Add other cleaning steps as necessary...
-        return df
+# You'll define methods for each data source in this class when required.
