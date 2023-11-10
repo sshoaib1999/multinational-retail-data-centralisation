1f69304fac0e35e7fb8a0f6bd2c5fb90059fc99a
sshoaib1999 <sshoaib1999@hotmail.com>
2023-11-10 21:47:16 +0000

    Delete task 5.py

--- task 5.py
+++
@@ -1,8 +0,0 @@
-from database_utils import DatabaseConnector
-from data_extraction import DataExtractor
-from data_cleaning import DataCleaning
-
-# Assuming you have cleaned store data as 'cleaned_store_data'
-table_name = 'dim_store_details'
-db_connector = DatabaseConnector(database_name='sales_data', user='postgres', password='Smsnn624', host='localhost', port='5432')
-db_connector.upload_to_db(cleaned_store_data, table_name)
